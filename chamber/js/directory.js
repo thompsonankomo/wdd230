d04623f2216a2f39d381a4807e5fa203fcca2c1f
thompsonankomo <nko20005@byui.edu>
2022-07-03 17:35:06 +0200

    commit

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,18 +1,49 @@
-const gridbutton = document.querySelector("#grid");
-const listbutton = document.querySelector("#list");
-const display = document.querySelector("article");
+const requestURL = 'scripts/data.json';
+const grid = document.querySelector('.grid');
 
-// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+function displaybusniess(busniess) {
+    // Create elements to add to the document
+    let card = document.createElement('section');
+    let img = document.createElement('img');
+    let url = document.createElement('a');
+    let name = document.createElement('h3');
+    let address = document.createElement('p');
+    let phone = document.createElement('p');
+    let membership = document.createElement('p');
 
-gridbutton.addEventListener("click", () => {
-	// example using arrow function
-	display.classList.add("grid");
-	display.classList.remove("list");
-});
+    // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
+    img.setAttribute('src', busniess.imageurl);
+    img.setAttribute('alt', `logo of ${busniess.name}`);
+    img.setAttribute('loading', 'lazy');
+    
+    // name, address and url of the busniess
+    name.textContent = `${busniess.name}`;
+    address.textContent = `${busniess.address}`;
+    phone.textContent = `${busniess.phone}`;
+    membership.textContent = `Membership Level: ${busniess.membership_level}`;
+    var linkText = document.createTextNode("Details");
+    url.appendChild(linkText);
+    url.title = "Details";
+    url.href = `${business.url}`;
 
-listbutton.addEventListener("click", showList); // example using defined function
+    // Add/append the section(card) with the h2 element
+    card.appendChild(img);
+    card.appendChild(name);
+    card.appendChild(address);
+    card.appendChild(phone);
+    card.appendChild(membership);
+    card.appendChild(url);
 
-function showList() {
-	display.classList.add("list");
-	display.classList.remove("grid");
+    // Add/append the existing HTML div with the cards class with the section(card)
+    grid.appendChild(card);
 }
+
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    
+    .then(function (jsonObject) {
+        const busniess = jsonObject['busniess'];
+        busniess.forEach(displaybusniess);
+    });
